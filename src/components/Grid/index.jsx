80f3273dae838d498df262bb1743838ddc7b3d7f
Gelo <tmarkangelo96@gmail.com>
2023-01-29 10:48:03 +0800

    feat: add blur effect on image load

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -5,7 +5,12 @@ export default function ({ data }) {
     <div className="main__grid">
       {images?.map((image, index) => (
         <div className="grid__item" key={`${image}-${index}`}>
-          <img className="item__image" src={image} alt={image} />
+          <img
+            onLoad={(event) => event.target.classList.remove("hide")}
+            className="item__image"
+            src={image}
+            alt={image}
+          />
         </div>
       ))}
     </div>
